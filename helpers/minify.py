3992da06852670d5818c2d26de79e4aaa354dbe8
slavakurilyak <hello@slavakurilyak.com>
2023-04-14 16:18:27 -0500

    Add helper script to minify python code using python-minifier

---
+++ helpers/minify.py
@@ -0,0 +1,18 @@
+import os
+import python_minifier
+
+root_dirs = ["agentx", "tests"]
+output_dir = ".minified"
+
+for root_dir in root_dirs:
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(".py"):
+                input_path = os.path.join(root, file)
+                output_path = os.path.join(output_dir, root_dir, os.path.relpath(root, root_dir), file)
+                output_dirname = os.path.dirname(output_path)
+                os.makedirs(output_dirname, exist_ok=True)
+                with open(input_path) as f:
+                    minified = python_minifier.minify(f.read())
+                with open(output_path, "w") as f:
+                    f.write(minified)
