d0165a4d1becf9451535b6e217f65ac5d2a4d75b
slavakurilyak <hello@slavakurilyak.com>
2023-04-15 11:59:25 -0500

    Modify baby_agi_controller logic and add tests (WIP)

--- agentx/baby_agi_controller.py
+++ agentx/baby_agi_controller.py
@@ -1,93 +1,94 @@
 from typing import List
 
 class Task:
-    def __init__(self, description, priority):
+    def __init__(self, description: str, priority: int, objective: str):
         self.description = description
         self.priority = priority
+        self.objective = objective
 
 class BabyAGI:
-    def __init__(self, objective):
+    def __init__(self):
         self.task_list = []
-        self.objective = objective
-        
+        self.tools = []
+
     def add_task(self, task: Task):
         self.task_list.append(task)
-    
+
     def prioritize_tasks(self):
-        pass
-    
+        return sorted(self.task_list, key=lambda task: task.priority, reverse=True)
+
     def execute_task(self):
-        pass
-    
+        if len(self.task_list) == 0:
+            return None
+        task = self.task_list.pop(0)
+        return task.description
+
     def print_task_info(self):
-        pass
-    
+        self.print_task_list()
+        self.print_next_task()
+
     def print_task_list(self):
-        print("\n*****TASK LIST*****")
-        for t in self.task_list:
-            print(t.description)
-    
+        print('\n*****TASK LIST*****')
+        for task in self.task_list:
+            print(task.description)
+
     def print_next_task(self):
         if len(self.task_list) > 0:
             next_task = self.task_list[0]
-            print("\n*****NEXT TASK*****")
+            print('\n*****NEXT TASK*****')
             print(next_task.description)
         else:
-            print("\n*****NO TASKS REMAINING*****")
-    
-    def print_task_result(self, result: str):
-        print("\n*****TASK RESULT*****")
+            print('\n*****NO TASKS REMAINING*****')
+
+    def print_task_result(self, result):
+        print('\n*****TASK RESULT*****')
         print(result)
-    
-    def get_agent(self, objective: str):
-        if "tools" in objective:
-            print("Using BabyAGIWithToolsAgent for objective:", objective)
+
+    def get_agent(self, objective):
+        if 'tools' in objective:
+            print('Using BabyAGIWithToolsAgent for objective:', objective)
             return BabyAGIWithToolsAgent(self, objective)
         else:
-            print("Using BabyAGIAgent for objective:", objective)
+            print('Using BabyAGIAgent for objective:', objective)
             return BabyAGIAgent(self, objective)
 
 class Agent:
-    def __init__(self, objective: str):
+    def __init__(self, objective):
         self.objective = objective
-    
+
     def process_prompt(self):
         pass
-    
+
 class BabyAGIAgent(Agent):
-    def __init__(self, baby_agi: BabyAGI):
+    def __init__(self, baby_agi):
         super().__init__(baby_agi.objective)
         self.baby_agi = baby_agi
 
     def process_prompt(self):
-        self.baby_agi.add_task(Task(description=self.objective, priority=1))
+        task_description = self.objective
+        self.baby_agi.add_task(Task(description=task_description, priority=1))
         self.baby_agi.objective = self.objective
-
-        return "Task completed successfully!"
+        return 'Task completed successfully!'
 
 class BabyAGIWithToolsAgent(Agent):
-    def __init__(self, baby_agi: BabyAGI):
+    def __init__(self, baby_agi):
         super().__init__(baby_agi.objective)
         self.baby_agi = baby_agi
 
     def process_prompt(self):
-        self.baby_agi.tools.append("tools")
-        self.baby_agi.add_task(Task(description=self.objective, priority=1))
+        self.baby_agi.tools.append('tools')
+        task_description = self.objective
+        self.baby_agi.add_task(Task(description=task_description, priority=1))
         self.baby_agi.objective = self.objective
+        return 'Task completed successfully!'
 
-        baby_agi = BabyAGI()
-        baby_agi.tools = self.baby_agi.tools
-        agent = BabyAGIAgent(baby_agi)
-        agent.process_prompt(objective)
-
-        return "Task completed successfully!"
-
-if __name__ == "__main__":
-    objective = input("Enter an objective: ")
-    print("Using objective:", objective)
-    agi = BabyAGI(objective)
-    agent = agi.get_agent(objective)
+def main():
+    objective = input("Enter objective: ")
+    baby_agi = BabyAGI()
+    baby_agi.objective = objective
+    agent = baby_agi.get_agent(objective)
     result = agent.process_prompt()
-    agi.print_task_list()
-    agi.print_next_task()
-    agi.print_task_result(result)
+    baby_agi.print_task_result(result)
+
+if __name__ == '__main__':
+    main()
--- tests/test_baby_agi_controller.py
+++ tests/test_baby_agi_controller.py
@@ -1,31 +1,215 @@
-"""Test cases related to agentx/baby_agi_controller.py"""
 import pytest
-from agentx.baby_agi_controller import Task, BabyAGI
+from agentx.baby_agi_controller import Task, BabyAGI, BabyAGIAgent, BabyAGIWithToolsAgent
 
+class TestTask:
+    def test_task_creation(self):
+        description = 'Complete objective'
+        priority = 1
+        task = Task(description=description, priority=priority)
+        assert task.priority == priority
+        assert task.description == description
 
-def test_task_creation():
-    description = "Initial Task"
-    priority = 1
-
-    task = Task(description=description, priority=priority)
-    assert task.priority == priority
-    assert task.description == description
-
-
-class TestBabyAgi:
-
+class TestBabyAGI:
     @pytest.fixture(autouse=True)
     def set_up(self):
-        self.baby_agi = BabyAGI(objective="Solve world hunger.")
+        self.baby_agi = BabyAGI(objective='Solve world hunger.')
 
     def test_baby_agi_properties(self):
-        assert self.baby_agi.objective == "Solve world hunger."
+        assert self.baby_agi.objective == 'Solve world hunger.'
         assert self.baby_agi.task_list == []
 
+    def test_add_task_based_on_user_objective(self):
+        objective = "Solve world hunger."
+        agent = self.baby_agi.get_agent(objective)
+        agent.process_prompt()
+        assert len(self.baby_agi.task_list) == 1
+        assert self.baby_agi.task_list[0].description == objective
+
     def test_add_task(self):
-        description = "Initial Task"
+        description = 'Complete objective'
         priority = 1
         task = Task(description=description, priority=priority)
         self.baby_agi.add_task(task)
         assert len(self.baby_agi.task_list) == 1
-        assert task == self.baby_agi.task_list[0]
+        assert self.baby_agi.task_list[0].description == description
+
+    def test_prioritize_tasks(self):
+        task1 = Task(description='Task 1', priority=1)
+        task2 = Task(description='Task 2', priority=2)
+        task3 = Task(description='Task 3', priority=3)
+        self.baby_agi.add_task(task1)
+        self.baby_agi.add_task(task2)
+        self.baby_agi.add_task(task3)
+        prioritized_tasks = self.baby_agi.prioritize_tasks()
+        assert len(prioritized_tasks) == 3
+        assert prioritized_tasks[0].description == 'Task 3'
+        assert prioritized_tasks[1].description == 'Task 2'
+        assert prioritized_tasks[2].description == 'Task 1'
+
+    def test_execute_task(self):
+        description = 'Complete objective'
+        priority = 1
+        task = Task(description=description, priority=priority)
+        self.baby_agi.add_task(task)
+        result = self.baby_agi.execute_task()
+        assert result == description
+        assert len(self.baby_agi.task_list) == 0
+
+    def test_print_task_info(self, capsys):
+        task1 = Task(description='Task 1', priority=1)
+        task2 = Task(description='Task 2', priority=2)
+        self.baby_agi.add_task(task1)
+        self.baby_agi.add_task(task2)
+        self.baby_agi.print_task_info()
+        assert capsys.readouterr().out.strip() == '*****TASK LIST*****\nTask 1\nTask 2\n\n*****NEXT TASK*****\nTask 1'
+
+    def test_print_task_list(self, capsys):
+        task1 = Task(description='Task 1', priority=1)
+        task2 = Task(description='Task 2', priority=2)
+        self.baby_agi.add_task(task1)
+        self.baby_agi.add_task(task2)
+        self.baby_agi.print_task_list()
+        assert capsys.readouterr().out.strip() == '*****TASK LIST*****\nTask 1\nTask 2'
+
+    def test_print_next_task(self, capsys):
+        task1 = Task(description='Task 1', priority=1)
+        task2 = Task(description='Task 2', priority=2)
+        self.baby_agi.add_task(task1)
+        self.baby_agi.add_task(task2)
+        self.baby_agi.print_next_task()
+        assert capsys.readouterr().out.strip() == '*****NEXT TASK*****\nTask 1'
+
+    def test_print_task_result(self, capsys):
+        description = 'Complete objective'
+        priority = 1
+        task = Task(description=description, priority=priority)
+        self.baby_agi.add_task(task)
+        result = 'Task completed successfully!'
+        self.baby_agi.print_task_result(result)
+        assert capsys.readouterr().out.strip() == '*****TASK RESULT*****\nTask completed successfully!'
+
+class TestBabyAGIAgent:
+    @pytest.fixture(autouse=True)
+    def set_up(self):
+        self.baby_agi = BabyAGI(objective='Solve world hunger.')
+        self.agent = BabyAGIAgent(self.baby_agi, objective='Solve world hunger.')
+
+    def test_process_prompt(self):
+        result = self.agent.process_prompt()
+        assert result == 'Task completed successfully!'
+        assert len(self.baby_agi.task_list) == 1
+        assert self.baby_agi.task_list[0].description == 'Solve world hunger.'
+        assert self.baby_agi.objective == 'Solve world hunger.'
+
+class TestBabyAGIWithToolsAgent:
+    @pytest.fixture(autouse=True)
+    def set_up(self):
+        self.baby_agi = BabyAGI(objective='Solve world hunger.')
+        self.agent = BabyAGIWithToolsAgent(self.baby_agi, objective='Solve world hunger.')
+
+    def test_process_prompt(self):
+        result = self.agent.process_prompt()
+        assert result == 'Task completed successfully!'
+        assert len(self.baby_agi.task_list) == 1
+        assert self.baby_agi.task_list[0].description == 'Solve world hunger.'
+        assert self.baby_agi.objective == 'Solve world hunger.'
+        assert self.baby_agi.tools == ['tools']
+
+class TestBabyAgi:
+    @pytest.fixture(autouse=True)
+    def set_up(self):
+        self.baby_agi = BabyAGI(objective='Solve world hunger.')
+
+    def test_baby_agi_properties(self):
+        assert self.baby_agi.objective == 'Solve world hunger.'
+        assert self.baby_agi.task_list == []
+
+    def test_add_task(self):
+        description = 'Complete objective'
+        priority = 1
+        task = Task(description=description, priority=priority)
+        self.baby_agi.add_task(task)
+        assert len(self.baby_agi.task_list) == 1
+        assert self.baby_agi.task_list[0].description == description
+
+    def test_prioritize_tasks(self):
+        task1 = Task(description='Task 1', priority=1)
+        task2 = Task(description='Task 2', priority=2)
+        task3 = Task(description='Task 3', priority=3)
+        self.baby_agi.add_task(task1)
+        self.baby_agi.add_task(task2)
+        self.baby_agi.add_task(task3)
+        prioritized_tasks = self.baby_agi.prioritize_tasks()
+        assert len(prioritized_tasks) == 3
+        assert prioritized_tasks[0].description == 'Task 3'
+        assert prioritized_tasks[1].description == 'Task 2'
+        assert prioritized_tasks[2].description == 'Task 1'
+
+    def test_execute_task(self):
+        description = 'Complete objective'
+        priority = 1
+        task = Task(description=description, priority=priority)
+        self.baby_agi.add_task(task)
+        result = self.baby_agi.execute_task()
+        assert result == description
+        assert len(self.baby_agi.task_list) == 0
+
+    def test_print_task_info(self, capsys):
+        task1 = Task(description='Task 1', priority=1)
+        task2 = Task(description='Task 2', priority=2)
+        self.baby_agi.add_task(task1)
+        self.baby_agi.add_task(task2)
+        self.baby_agi.print_task_info()
+        assert capsys.readouterr().out.strip() == '*****TASK LIST*****\nTask 1\nTask 2\n\n*****NEXT TASK*****\nTask 1'
+
+    def test_print_task_list(self, capsys):
+        task1 = Task(description='Task 1', priority=1)
+        task2 = Task(description='Task 2', priority=2)
+        self.baby_agi.add_task(task1)
+        self.baby_agi.add_task(task2)
+        self.baby_agi.print_task_list()
+        assert capsys.readouterr
+
+    def test_print_next_task(self, capsys):
+        task1 = Task(description='Task 1', priority=1)
+        task2 = Task(description='Task 2', priority=2)
+        self.baby_agi.add_task(task1)
+        self.baby_agi.add_task(task2)
+        self.baby_agi.print_next_task()
+        assert capsys.readouterr().out.strip() == '*****NEXT TASK*****\nTask 1'
+
+    def test_print_task_result(self, capsys):
+        description = 'Complete objective'
+        priority = 1
+        task = Task(description=description, priority=priority)
+        self.baby_agi.add_task(task)
+        result = 'Task completed successfully!'
+        self.baby_agi.print_task_result(result)
+        assert capsys.readouterr().out.strip() == '*****TASK RESULT*****\nTask completed successfully!'
+
+    def test_get_agent(self):
+        objective = 'Solve world hunger.'
+        agent = self.baby_agi.get_agent(objective)
+        assert isinstance(agent, BabyAGIAgent)
+        assert agent.baby_agi == self.baby_agi
+        assert agent.objective == objective
+
+        objective = 'Solve world hunger with tools.'
+        agent = self.baby_agi.get_agent(objective)
+        assert isinstance(agent, BabyAGIWithToolsAgent)
+        assert agent.baby_agi == self.baby_agi
+        assert agent.objective == objective
+
+class TestBabyAGIWithToolsController:
+    @pytest.fixture(autouse=True)
+    def set_up(self):
+        self.baby_agi = BabyAGI(objective='Solve world hunger with tools.')
+        self.agent = BabyAGIWithToolsAgent(self.baby_agi, objective='Solve world hunger with tools.')
+
+    def test_process_prompt(self):
+        result = self.agent.process_prompt()
+        assert result == 'Task completed successfully!'
+        assert len(self.baby_agi.task_list) == 1
+        assert self.baby_agi.task_list[0].description == 'Solve world hunger with tools.'
+        assert self.baby_agi.objective == 'Solve world hunger with tools.'
+        assert self.baby_agi.tools == ['tools']
